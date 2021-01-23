00b985cfa42c901c4e258c76d7d8ae5d294c9a96
Vili <vili@consolidity.com>
2021-01-23 17:17:53 +0100

    remove unnecessary code and change titles

--- src/index.ts
+++ src/index.ts
@@ -7,33 +7,18 @@ const path = require('path');
 const program = require('commander');
 
 clear();
-console.log(
-  chalk.red(
-    figlet.textSync('pizza-cli', { horizontalLayout: 'full' })
-  )
-);
+console.log(chalk.red(figlet.textSync('friday-cli', { horizontalLayout: 'full' })));
 
 program
-	.version('0.0.1')
-  .description("An example CLI for ordering pizza's")
-  .option('-p, --peppers', 'Add peppers')
-  .option('-P, --pineapple', 'Add pineapple')
-  .option('-b, --bbq', 'Add bbq sauce')
-  .option('-c, --cheese <type>', 'Add the specified type of cheese [marble]')
-  .option('-C, --no-cheese', 'You do not want any cheese')
-	.parse(process.argv);
-	
-console.log('you ordered a pizza with:');
-if (program.peppers) console.log('  - peppers');
-if (program.pineapple) console.log('  - pineapple');
-if (program.bbq) console.log('  - bbq');
+  .version('0.0.1')
+  .description('Friday CLI')
+  .option('-t, --thirteen', 'get the nearest friday the 13th')
+  .parse(process.argv);
 
-const cheese: string = true === program.cheese
-	? 'marble'
-	: program.cheese || 'no';
-
-console.log('  - %s cheese', cheese);
+if (program.thirteen) {
+  //return the date of the nearest friday the 13th
+}
 
 if (!process.argv.slice(2).length) {
-	program.outputHelp();
+  program.outputHelp();
 }
