99e03f2745281a2c6595ce36357460ccb866c115
Vili <vili@consolidity.com>
2021-01-23 17:30:59 +0100

    addDays is working

--- src/friday.test.ts
+++ src/friday.test.ts
@@ -9,7 +9,20 @@ test('getNextFridayThe13th: if from is 2021-01-23 it should return with 2021-08-
   //act
   const result = friday.getNextFridayThe13th(from);
 
-  //asser
+  //assert
   t.equal(+result, +new Date('2021-08-13'));
+  t.end();
+});
+
+test('addDays: if date is 2021-01-23 and 1 day added it should return 2021-01-24', (t: any) => {
+  //arrange
+  const friday = new Friday();
+  const date = new Date('2021-01-23');
+
+  //act
+  const result = friday.addDays(date, 1);
+
+  //assert
+  t.equal(+result, +new Date('2021-01-24'));
   t.end();
 });
--- src/friday.ts
+++ src/friday.ts
@@ -1,5 +1,17 @@
 export class Friday {
   getNextFridayThe13th(from: Date): Date {
-    return new Date();
+    let date = new Date(from);
+    //base case
+    if (date.getDate() === 13 && date.getDay() === 5) {
+      return date;
+    }
+    date = this.addDays(date, 1);
+    return this.getNextFridayThe13th(date);
+  }
+
+  addDays(date: Date, days: number) {
+    const result = new Date(date);
+    result.setDate(result.getDate() + days);
+    return result;
   }
 }
