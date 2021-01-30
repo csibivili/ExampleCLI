0378e0642e7120093b1b812583cf05cf18143b26
Vili <vili@consolidity.com>
2021-01-30 16:42:16 +0100

    new build

---
+++ lib/friday/friday.d.ts
@@ -0,0 +1,5 @@
+export declare class Friday {
+    getNextFridayThe13th(from: Date): Date;
+    getDateOfBlackFridayByYear(year: number): Date;
+    addDays(date: Date, days: number): Date;
+}
---
+++ lib/validator/validator.d.ts
@@ -0,0 +1,4 @@
+export declare class Validator {
+    isValidDate(input: string): string;
+    isValidInteger(input: string): number;
+}
