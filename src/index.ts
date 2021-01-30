f680eade37c5873bbc73ee959e4f08fbe4db039c
Vili <vili@consolidity.com>
2021-01-30 11:02:50 +0100

    second arg

--- src/index.ts
+++ src/index.ts
@@ -15,12 +15,17 @@ program
   .version('0.0.1')
   .description('Friday CLI')
   .option('-t, --thirteen', 'get the nearest friday the 13th')
+  .option('-b, --black', 'get the black friday of the given year')
   .parse(process.argv);
 
 if (program.thirteen) {
   const friday = new Friday();
   const nextFridayThe13th = friday.getNextFridayThe13th(program.args[0] ? new Date(program.args[0]) : new Date());
   console.log(nextFridayThe13th.toLocaleDateString('hu-HU'));
+}
+
+if (program.black) {
+  console.log(program.opts().black);
 }
 
 if (!process.argv.slice(2).length) {
